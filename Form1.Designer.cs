4c3f63822d3583c7bf53ad5cb4781e5d60b56361
LAPTOP-Q9IMV3R2\wan <q020385791@gmail.com>
2023-12-06 18:06:09 +0800

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace ExportSchma
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtConnectionString = new TextBox();
+            btnExport = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // txtConnectionString
+            // 
+            txtConnectionString.Location = new Point(123, 8);
+            txtConnectionString.Name = "txtConnectionString";
+            txtConnectionString.Size = new Size(431, 23);
+            txtConnectionString.TabIndex = 0;
+            // 
+            // btnExport
+            // 
+            btnExport.Location = new Point(479, 37);
+            btnExport.Name = "btnExport";
+            btnExport.Size = new Size(75, 23);
+            btnExport.TabIndex = 1;
+            btnExport.Text = "匯出";
+            btnExport.UseVisualStyleBackColor = true;
+            btnExport.Click += btnExport_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 16);
+            label1.Name = "label1";
+            label1.Size = new Size(105, 15);
+            label1.TabIndex = 2;
+            label1.Text = "ConnectionString";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(566, 71);
+            Controls.Add(label1);
+            Controls.Add(btnExport);
+            Controls.Add(txtConnectionString);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtConnectionString;
+        private Button btnExport;
+        private Label label1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+using Microsoft.Office.Interop.Excel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Runtime.InteropServices;
+using DataTable = System.Data.DataTable;
+using Range = Microsoft.Office.Interop.Excel.Range;
+
+namespace ExportSchma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            string connectionString = txtConnectionString.Text;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    // Retrieve table names from the database
+                    DataTable tableSchema = connection.GetSchema("Tables");
+
+                    // Create Excel application
+                    Microsoft.Office.Interop.Excel.Application excelApp = new Microsoft.Office.Interop.Excel.Application();
+                    Workbook workbook = excelApp.Workbooks.Add();
+
+                    foreach (DataRow tableRow in tableSchema.Rows)
+                    {
+                        string tableName = tableRow["TABLE_NAME"].ToString();
+
+                        // Retrieve column comments for each table
+                        DataTable columnComments = GetColumnComments(connection, tableName);
+
+                        // Export data to the current workbook
+                        ExportToExcel(workbook, tableName, columnComments);
+                    }
+
+                    // Save the Excel file
+                    workbook.SaveAs("ColumnComments.xlsx");
+                    workbook.Close();
+                    excelApp.Quit();
+
+                    MessageBox.Show("Data exported to Excel successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+            }
+
+        }
+
+
+        private DataTable GetColumnComments(SqlConnection connection, string tableName)
+        {
+            string query = @"
+        SELECT
+            c.COLUMN_NAME,
+            c.DATA_TYPE,
+            c.CHARACTER_MAXIMUM_LENGTH,
+            ep.value AS COLUMN_DESCRIPTION
+        FROM
+            INFORMATION_SCHEMA.COLUMNS c
+            LEFT JOIN sys.extended_properties ep ON ep.major_id = OBJECT_ID(c.TABLE_NAME) AND ep.minor_id = COLUMNPROPERTY(OBJECT_ID(c.TABLE_NAME), c.COLUMN_NAME, 'ColumnId')
+        WHERE
+            c.TABLE_NAME = @TableName";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@TableName", tableName);
+            // Create a DataTable to hold the results
+            DataTable dataTable = new DataTable();
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                dataTable.Load(reader);
+            }
+
+            return dataTable;
+        }
+        private void ExportToExcel(Workbook workbook, string tableName, DataTable dataTable)
+        {
+            // Add a new worksheet with the table name
+            Worksheet worksheet = (Worksheet)workbook.Sheets.Add();
+            worksheet.Name = tableName;
+            List<string> ColumnName = new List<string> { "欄位名稱","型別","長度","說明" };
+            // Write column names to the Excel sheet
+            for (int i = 0; i < dataTable.Columns.Count; i++)
+            {
+                worksheet.Cells[1, i + 1] = ColumnName[i];
+            }
+
+            // Write data to the Excel sheet
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                for (int j = 0; j < dataTable.Columns.Count; j++)
+                {
+                    worksheet.Cells[i + 2, j + 1] = dataTable.Rows[i][j].ToString();
+                }
+            }
+
+            // Auto-adjust column widths
+            AutoFitColumns(worksheet);
+        }
+        private void AutoFitColumns(Worksheet worksheet)
+        {
+            // Get the used range of the worksheet
+            Range usedRange = worksheet.UsedRange;
+
+            // AutoFit columns for the used range
+            usedRange.Columns.AutoFit();
+
+            // Release the used range object to avoid memory leaks
+            Marshal.ReleaseComObject(usedRange);
+        }
+    }
+}
